c110cfebde19eb2f1625c825fee006ca06a84f56
Griever <radical.griever@gmail.com>
2022-01-29 16:16:59 +0300

    0.5.0.0 - answer frequency setting is added, bug fixes
    0.5.0.1 - bug fix (column name extraction)
    
    0.5.0.3 - test
    
    0.5.0.4
    
    fixes

--- korni.py
+++ korni.py
@@ -6,6 +6,8 @@ import sys
 import random
 import urllib.parse as urlparse
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import ChatMember
+
 try:
     import config
 except ModuleNotFoundError:
@@ -17,35 +19,187 @@ morph = pymorphy2.MorphAnalyzer()
 
 
 def message_how(update, context):
-    # Send a message when the command /start is issued.
+    # Send a message when the command /kak is issued.
     update.message.reply_text('Способы использования:\nНаилучший способ - добавить (ро)бота к себе в болталки (беседы),'
-                            ' тогда он будет поправлять всех участников. Для "супергрупп" нужны права заведующего.'
+                            ' тогда он будет поправлять всех участников. Для "супергрупп" необходимы права заведующего.'
                             '\n\nУпрощённый способ - просто присылать любые письмена боту в личку, он тоже будет'
                             ' их поправлять. Но придётся каждый раз вручную это делать.')
 
+
 def message_info(update, context):
-    # Send a message when the command /start is issued.
+    # Send a message when the command /sved is issued.
     update.message.reply_text('Дополнительные сведения можно изведать по ссылке:\n'
                             'https://telegra.ph/Robot-popravlyalshchik-dlya-Telegrama-Korni-russkogo-04-10')
 
 
-# Let's analyze all the incoming text
-def process_text(update, context):
-    # connect to database
+def connect_to_db():
     try:
         if 'DATABASE_URL' in os.environ:  # if prod
             url = urlparse.urlparse(os.environ['DATABASE_URL'])
-            conn = psycopg2.connect(dbname=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+            connect = psycopg2.connect(dbname=url.path[1:], user=url.username, password=url.password,
+                                    host=url.hostname, port=url.port)
         else:
-            conn = psycopg2.connect(dbname=config.db_name, user=config.db_user, password=config.db_password, host=config.db_host)
+            connect = psycopg2.connect(dbname=config.db_name, user=config.db_user, password=config.db_password,
+                                    host=config.db_host)
     except psycopg2.OperationalError as e:
         print('Unable to connect!\n{0}').format(e)
-        sys.exit(1)
-    else:
-        pass
-    cursor = conn.cursor()
+        connect = None
+    finally:
+        return connect
 
+
+def get_db_frequency(cht_id):
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+    else:
+        sys.exit(1)
+    cursor.execute("SELECT freq FROM freq_data WHERE chat_id='" + str(cht_id) + "'")
+    res = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if res is not None:
+        #print("GET FREQ: " + str(res[0]))
+        return res[0]
+    else:
+        return res
+
+
+def change_react_frequency(update, context):
+    def send_message_when_wrong_argument():
+        update.message.reply_text("Используйте целое числовое значение в промежутке от 1 до 50 в строке после приказа "
+                                  "/vzvod и пробела для настройки ретивости робота в данной болталке.\nНапример: "
+                                  "\"/vzvod 1\" - взводиться всегда, \"/vzvod 2\" - взводиться на каждое второе "
+                                  "сообщение, \"/vzvod 10\" - взводиться на каждое десятое и т.п.")
+    # Process when the command /vzvod is issued.
+    if update.message is None:
+        return
+    if update.message.chat.type == "private":
+        update.message.reply_text("Настройка ретивости робота доступна только при использовании в болталках,"
+                                  " а не в личке!")
+        return
+    else:
+        if context.bot.getChatMember(update.effective_chat.id, update.effective_user.id).status not in \
+                                    [ChatMember.ADMINISTRATOR, ChatMember.CREATOR]:
+            update.message.reply_text("Настройка ретивости робота доступна лишь пользователям с правами заведующего!")
+            return
+    if len(context.args) > 0:
+        try:
+            param = int(context.args[0])
+        except ValueError:
+            send_message_when_wrong_argument()
+            return
+    else:
+        send_message_when_wrong_argument()
+        return
+    if (param < 1) | (param > 50):
+        send_message_when_wrong_argument()
+        return
+    set_db_frequency(param, update)
+    update.message.reply_text("Ретивость робота в данной болталке установлена на " +
+                              f'{100/param:4.2f}'.replace('.', ',') + "%")
+
+
+def set_db_frequency(fq, update):
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+    else:
+        sys.exit(1)
     # get column names
+    #cursor.execute("SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'freq_data'")
+    #records = cursor.fetchall()
+    id_chat_id = "chat_id" #records[0][0]
+    id_chat_caption = "chat_caption" #records[2][0]
+    id_chat_username = "username" #records[3][0]
+    id_freq = "freq" #records[1][0]
+    #print(records)
+
+    chat_id = update.effective_chat.id
+    title = update.effective_chat.title
+    if title is None:
+        title = "PRIVATE" if (update.message.chat.type == "private") else "NONE"
+
+    username = update.effective_chat.username
+    if username is None:
+        if update.message.chat.type == "private":
+            user_username = update.effective_user.username
+            username = "@" + user_username if (user_username is not None) else update.effective_user.first_name
+        else:
+            username = "NONE"
+    cursor.execute(
+        "INSERT INTO freq_data(" + id_chat_id + "," + id_chat_caption + "," + id_chat_username + "," + id_freq
+        + ") VALUES(" + str(chat_id) + ", '" + title + "', '" + username + "', " + str(fq) + ") " +
+        "ON CONFLICT (" + id_chat_id + ") DO UPDATE SET " +
+        "(" + id_chat_caption + ", " + id_chat_username + ", " + id_freq + ") = (" +
+        "EXCLUDED." + id_chat_caption + ", " +
+        "EXCLUDED." + id_chat_username + ", " +
+        "EXCLUDED." + id_freq + ")")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+# Let's analyze all the incoming text
+def process_text(update, context):
+    #print(datetime.datetime.now())
+    if update.message is None:
+        return
+    """chat_type = update.message.chat.type
+    print("chat type: " + chat_type)"""
+    chat_id = update.effective_chat.id
+    #print("chat id: " + str(chat_id))
+
+    current_freq = get_db_frequency(chat_id)
+    if current_freq is None:
+        #print("current freq is undefined")
+        current_freq = 1
+        set_db_frequency(current_freq, update)
+    #else:
+        #print("current freq is already set to " + str(current_freq))
+
+    is_no_message_process = (random.randint(1, current_freq) != 1)
+    #print("EXIT without processing " + str(is_no_message_process))
+    if is_no_message_process & (update.message.chat.type != "private"):
+        return
+
+    """username = update.effective_chat.username
+    print("chat username: " + (username if (username is not None) else "EMPTY"))
+    title = update.effective_chat.title
+    print("chat title: " + (title if (title is not None) else "EMPTY"))
+    user_id = update.effective_user.id
+    print("user trigger id: " + str(user_id if (user_id is not None) else "EMPTY")) #it's user id not bot id
+    user_username = update.effective_user.username
+    print("user trigger name: " + ("@"+str(user_username) if (user_username is not None) else update.effective_user.first_name))  # it's user id not bot id
+    bot_id = context.bot.id
+    print("bot id: " + str(bot_id if (bot_id is not None) else "EMPTY"))
+    bot_name = context.bot.name
+    print("bot name: " + bot_name)
+    bot_chat_member = context.bot.getChatMember(chat_id, bot_id)
+    if bot_chat_member.status == telegram.ChatMember.RESTRICTED:
+        perm = bot_chat_member.can_send_messages
+        print("bot perm can_send_messages: " + str(perm))
+    else:
+        print("no restrictions")"""
+
+    text_to_split = update.message.caption if (update.message.text is None) else update.message.text
+
+    # checks before processing
+    if update.message.chat.type != "private":
+        bot_chat_member = context.bot.getChatMember(update.effective_chat.id, context.bot.id)
+        if bot_chat_member.status == ChatMember.RESTRICTED:
+            if not bot_chat_member.can_send_messages:
+                return
+    else:
+        if text_to_split == "/start":
+            message_how(update, context)
+            return
+
+    conn = connect_to_db()
+    if conn is not None:
+        cursor = conn.cursor()
+    else:
+        sys.exit(1)
     cursor.execute("SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'rodno_data'")
     records = cursor.fetchall()
     id_non_native = records[0][0]
@@ -53,8 +207,6 @@ def process_text(update, context):
     id_exclusions = records[2][0]
 
     output_message = ""
-
-    text_to_split = update.message.caption if (update.message.text is None) else update.message.text
 
     # let's split it by words using re.sub(pattern, repl, string, count=0, flags=0)
     # [\w] means any alphanumeric character and is equal to the character set [a-zA-Z0-9_]
@@ -88,8 +240,8 @@ def process_text(update, context):
         output_message += "\n"
         lines = open("data/answers.txt", "r", encoding="utf-8").readlines()
         lines_ex = open("data/answers_extra.txt", "r", encoding="utf-8").readlines()
-        rnd_val = random.randint(1, len(lines))
-        rnd_extra = random.randint(1, len(lines_ex))
+        rnd_val = random.randint(0, len(lines)-1)
+        rnd_extra = random.randint(0, len(lines_ex)-1)
         if (update.message.from_user.username == 'Tatsuya_S') and (update.message.chat.type != 'private'):
             if "#" in lines_ex[rnd_extra]:
                 lines_ex[rnd_extra] = lines_ex[rnd_extra].replace("#", update.message.from_user.first_name if random.randint(1, 2) == 1 else "@" + update.message.from_user.username)
@@ -98,7 +250,7 @@ def process_text(update, context):
             output_message += lines[0]
         else:
             if "#" in lines[rnd_val]:
-                lines[rnd_val] = lines[rnd_val].replace("#", update.message.from_user.first_name if random.randint(1, 2) == 1 else "@" + update.message.from_user.username)
+                lines[rnd_val] = lines[rnd_val].replace("#", update.message.from_user.first_name if (random.randint(1, 2) == 1) | (update.message.from_user.username is None) else "@" + update.message.from_user.username)
             output_message += lines[rnd_val]
         update.message.reply_text(output_message)
     elif update.message.chat.type == 'private':
@@ -113,9 +265,12 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler('how', message_how))
-    dp.add_handler(CommandHandler('info', message_info))
-    dp.add_handler(MessageHandler((Filters.text | Filters.caption) & ((~Filters.forwarded) | Filters.private), process_text, pass_user_data=True))
+    dp.add_handler(CommandHandler('kak', message_how))
+    dp.add_handler(CommandHandler('sved', message_info))
+    dp.add_handler(CommandHandler('vzvod', change_react_frequency))
+    dp.add_handler(MessageHandler((Filters.text | Filters.caption) & ((~Filters.forwarded) & (~Filters.chat_type.channel) | Filters.chat_type.private),
+                                  process_text, pass_user_data=True))
+
 
     if 'TG_API_KEY' in os.environ:  # if prod
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=os.getenv("TG_API_KEY"),
